fcbf9597baa70ba0becfc538f5f2b40d2b4e3a5a
niranjana687 <niranjana687@gmail.com>
2021-09-02 00:01:05 +0530

    customised views

--- src/app.js
+++ src/app.js
@@ -6,9 +6,40 @@ const app = express();
 
 const port = 3000;
 
+//Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../views');
 
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+//home page route using hbs
+app.get('', (request, response) => {
+    response.render('index', {
+        title: 'Humor-It',
+        name: 'Niranjana',
+    });
+});
+
+//about page router
+app.get('/about', (request, response) => {
+    response.render('about', {
+        title: 'Humor It',
+        description: 'Weather app but with a funny twist!',
+        name: 'Niranjana',
+    });
+});
+
+//help route 
+app.get('/help', (request, response) => {
+    response.render('help', {
+        message: 'Help page of Humor it',
+    });
+});
 
 //weather route
 app.get('/weather', (request, response) => {
@@ -17,6 +48,7 @@ app.get('/weather', (request, response) => {
         location: 'kochi',
     });
 });
+
 
 
 app.listen(port, () => {
